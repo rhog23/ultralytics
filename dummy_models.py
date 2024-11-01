38fcd20f6724fa380e8ce05eb9c264727c3a6917
rhog23 <raymonde.saragih@gmail.com>
2024-11-01 15:17:28 +0700

    added note to yolo11-gam; added new yolo11 p2,p3,p4 head

--- dummy_models.py
+++ dummy_models.py
@@ -1,6 +1,6 @@
 from ultralytics.nn.tasks import DetectionModel
 
-det_model = DetectionModel(cfg="yolo11-gam.yaml")
+det_model = DetectionModel(cfg="yolo11-p2,4.yaml")
 
 # from ultralytics import YOLO
 
