539ec0e8f65175a724f44b6b248d0367ea7f18de
rhog23 <raymonde.saragih@gmail.com>
2024-11-15 21:29:19 +0700

    added yolov10s-p2.yaml

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 from ultralytics.utils.torch_utils import fuse_conv_and_bn
 
-from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad, SimAM
+from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad, SimAM, CBAM
 from .transformer import TransformerBlock
 
 __all__ = (
@@ -29,6 +29,7 @@ __all__ = (
     "C3Ghost",
     "GhostBottleneck",
     "Bottleneck",
+    "BottleneckCBAM",
     "BottleneckCSP",
     "Proto",
     "RepC3",
@@ -376,6 +377,27 @@ class Bottleneck(nn.Module):
     def forward(self, x):
         """Applies the YOLO FPN to input data."""
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class BottleneckCBAM(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a standard bottleneck module with optional shortcut connection and configurable parameters."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+        self.cbam = CBAM(c_)
+
+    def forward(self, x):
+        """Applies the YOLO FPN to input data."""
+        return (
+            x + self.cbam(self.cv2(self.cv1(x)))
+            if self.add
+            else self.cbam(self.cv2(self.cv1(x)))
+        )
 
 
 class BottleneckCSP(nn.Module):
@@ -1235,6 +1257,32 @@ class C2fSimAM(nn.Module):
         self.cv2 = nn.Sequential(Conv((2 + n) * self.c, c2, 1), SimAM())
         self.m = nn.ModuleList(
             BottleneckSimAM(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0)
+            for _ in range(n)
+        )
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+class C2fCBAM(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(
+            Bot(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0)
             for _ in range(n)
         )
 
