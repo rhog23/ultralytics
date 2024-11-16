0eae91578adc6edacbeb4bd32ec0e9eb4d3b48d2
rhog23 <raymonde.saragih@gmail.com>
2024-11-16 22:45:11 +0700

    added yolo11-p2

--- dummy_models.py
+++ dummy_models.py
@@ -1,7 +1,9 @@
 from ultralytics.nn.tasks import DetectionModel
 
-det_model = DetectionModel(cfg="yolo11l-p2,4.yaml")
-det_model.info()
+det_model = DetectionModel(cfg="yolo11m-p2.yaml")
+det_model = DetectionModel(cfg="yolo11l-p2.yaml")
+det_model = DetectionModel(cfg="yolo11x-p2.yaml")
+# det_model.info()
 
 # from ultralytics import YOLO
 
