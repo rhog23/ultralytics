689fb0f695c25db446928a2752dc68dfd9570baa
rhog23 <raymonde.saragih@gmail.com>
2024-11-16 23:58:28 +0700

    update yolo11-gam

--- dummy_models.py
+++ dummy_models.py
@@ -1,8 +1,6 @@
 from ultralytics.nn.tasks import DetectionModel
 
-det_model = DetectionModel(cfg="yolo11m-p2.yaml")
-det_model = DetectionModel(cfg="yolo11l-p2.yaml")
-det_model = DetectionModel(cfg="yolo11x-p2.yaml")
+det_model = DetectionModel(cfg="yolov9s.yaml")
 # det_model.info()
 
 # from ultralytics import YOLO
